beb012366f966c7c818eda4501cc78b7d8ed6b61
tyler7kehoe <kehoe.tyler@att.net>
2022-04-03 12:04:33 -0500

    Reaction Roles working :+1:

--- cogs/reaction_roles.py
+++ cogs/reaction_roles.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 from discord.ext import commands
 from discord.ext.commands.context import Context
@@ -5,8 +6,6 @@ from dotenv import load_dotenv
 import json
 
 load_dotenv()
-
-global channel_id, emoji_list, rolls_list, role_dict
 
 
 class reaction_roles(commands.Cog):
@@ -16,32 +15,36 @@ class reaction_roles(commands.Cog):
     @commands.has_permissions(manage_webhooks=True)
     @commands.slash_command(name="reaction_roles", description="Click reactions for reaction roles")
     async def reactionroles(self, ctx: Context):
-        global channel_id, emoji_list, rolls_list, role_dict
 
         def check(m):
             return m.content and m.channel == ctx.channel
-        await ctx.respond('What roles will be included in the reaction roles embed?')
-        rolls = await self.bot.wait_for("message", check=check)
-        rolls_list = rolls.content.split()
+        try:
+            await ctx.respond('What roles will be included in the reaction roles embed?')
+            rolls = await self.bot.wait_for("message", check=check, timeout=240)
+            rolls_list = rolls.content.split()
 
-        await ctx.respond('What emojis would you like to use for the users to select their roles? Please have the emojis'
-                          ' separated by a space and correspond to the order you listed the roles')
-        proper_length = False
-        while not proper_length:
-            emojis = await self.bot.wait_for("message", check=check)
-            emojis = emojis.content.split()
-            if len(emojis) == len(rolls_list):
-                proper_length = True
-            else:
-                await ctx.respond('Ensure you enter the correct number of emojis per role')
+            await ctx.respond('What emojis would you like to use for the users to select their roles? Please have the emojis'
+                              ' separated by a space and correspond to the order you listed the roles')
+            proper_length = False
+            while not proper_length:
+                emojis = await self.bot.wait_for("message", check=check, timeout=240)
+                emojis = emojis.content.split()
+                if len(emojis) == len(rolls_list):
+                    proper_length = True
+                else:
+                    await ctx.respond('Ensure you enter the correct number of emojis per role')
 
-        emoji_list = emojis
-        await ctx.respond('What channel would you like the embed to appear in? (Please input as #channel-name)')
-        chID_msg = await self.bot.wait_for("message", check=check)
+            emoji_list = emojis
+            await ctx.respond('What channel would you like the embed to appear in? (Please input as #channel-name)')
+            chID_msg = await self.bot.wait_for("message", check=check, timeout=240)
+        except asyncio.TimeoutError:
+            await ctx.respond('You took too long to respond to the message (Timeout after 4 minutes.)')
+        except ValueError:
+            await ctx.repond('There was likely an input error. Please try again')
+
         chID_stripped = chID_msg.content[2:]
         chID_stripped = chID_stripped[:-1]
         chID = self.bot.get_channel(int(chID_stripped))
-        channel_id = chID
 
         embed = discord.Embed(title="Select Roles!", color=discord.Color.brand_red())
 
@@ -56,43 +59,51 @@ class reaction_roles(commands.Cog):
         await self.log_msg(reaction_roles_msg.id, reaction_roles_msg.channel.id, rolls_list, emoji_list)
 
     @commands.Cog.listener()
-    async def on_reaction_add(self, reaction, user):
-        global channel_id, emoji_list, rolls_list, role_dict
-
-        if reaction.message.channel.id != channel_id.id:
-            return
-        role_name = role_dict[reaction.emoji]
-        role = discord.utils.get(reaction.message.guild.roles, name=role_name)
-        if user != self.bot.user:
-            await user.add_roles(role)
-
-    @commands.Cog.listener()
-    async def on_reaction_remove(self, reaction, user):
-        global channel_id
-        if reaction.message.channel.id != channel_id.id:
-            return
-        role_name = role_dict[reaction.emoji]
-        role = discord.utils.get(reaction.message.guild.roles, name=role_name)
-        await user.remove_roles(role)
-
-    # TODO: Create JSON, input message/channel id's as well as corresponding roles and emojis.
-
-    @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload):
+    async def on_raw_reaction_remove(self, payload):
+        obj = None
         message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
-        with open("reaction_roles.json", "r") as _:
+        guild = await self.bot.fetch_guild(payload.guild_id)
+        member = await guild.fetch_member(payload.user_id)
+        emoji = str(payload.emoji.id) if payload.emoji.is_custom_emoji() else payload.emoji.name
+        with open("reaction_roles.json", "r", encoding="UTF-8") as _:
             data = json.load(_)
             for item in data:
                 if item["message_id"] == message.id:
                     obj = item
-        # if await message.channel.fetch_message(payload.message_id) != obj["message_id"]:
-        #     return
-        role = "role1"
+            if obj is None:
+                return
+        num = -1
+        emList = obj['emojis']
+        for i in range(len(emList)):
+            if emList[i] == emoji:
+                num = i
+        role = discord.utils.get(message.guild.roles, name=obj["roles"][num])
+        if member != self.bot.user:
+            await member.remove_roles(role)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        obj = None
+        message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+        emoji = str(payload.emoji.id) if payload.emoji.is_custom_emoji() else payload.emoji.name
+        with open("reaction_roles.json", "r", encoding="UTF-8") as _:
+            data = json.load(_)
+            for item in data:
+                if item["message_id"] == message.id:
+                    obj = item
+            if obj is None:
+                return
+        num = -2
+        emList = obj['emojis']
+        for i in range(len(emList)):
+            if emList[i] == emoji:
+                num = i
+        role = discord.utils.get(message.guild.roles, name=obj["roles"][num])
         if payload.member != self.bot.user:
             await payload.member.add_roles(role)
 
     async def log_msg(self, message_id, ch_id, roles, emojis):
-        with open("reaction_roles.json", "r") as _:
+        with open("reaction_roles.json", "r", encoding="UTF-8") as _:
             data = json.load(_)
 
             new_set = {
@@ -105,7 +116,7 @@ class reaction_roles(commands.Cog):
             if new_set not in data:
                 data.append(new_set)
 
-        with open("reaction_roles.json", "w") as _:
+        with open("reaction_roles.json", "w", encoding="UTF-8") as _:
             json.dump(obj=data, fp=_, indent=4)
 
 
