616eb5a3773b80eec52d71fff5b506e3e6ab1713
tyler7kehoe <kehoe.tyler@att.net>
2022-04-03 12:11:54 -0500

    Create setup/webhook del

---
+++ cogs/setup.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands.context import Context
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Setup(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+def setup(bot):
+    bot.add_cog(Setup(bot))
+    
