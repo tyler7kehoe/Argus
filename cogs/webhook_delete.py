2304ad1d6016936473a160f7698cbea4a0c8fff9
tyler7kehoe <kehoe.tyler@att.net>
2022-04-05 21:38:57 -0500

    Webhook Deletion Working
    IF ANYONE sends a webhook it will delete it

--- cogs/webhook_delete.py
+++ cogs/webhook_delete.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands.context import Context
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,6 +8,34 @@ load_dotenv()
 class WebhookDelete(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
+
+    @commands.Cog.listener('on_message')
+    async def del_webhook(self, message):
+        if message.webhook_id is None:
+            return
+        elif message.type is discord.MessageType.application_command:
+            return
+        else:
+            wh = await self.bot.fetch_webhook(message.webhook_id)
+            audit_logs = await wh.guild.audit_logs(limit=100).flatten()
+            await wh.delete(reason='Illegal webhook use')
+            await message.delete()
+            for entry in audit_logs:
+                if wh is not None:
+                    if entry.action is not None and entry.action is discord.AuditLogAction.webhook_create:
+                        if entry.target.id == wh.id:
+                            if entry.user is not None and entry.user.id is not self.bot.user:
+                                member = await wh.guild.fetch_member(entry.user.id)
+                                if member is not None:
+                                    mem_list = member.roles
+                                    mem_list.pop(0)
+                                    for role in mem_list:
+                                        await member.remove_roles(role, reason=f'Created webhook {entry.id}')
+                                    for channel in member.guild.channels:
+                                        await channel.set_permissions(member, send_messages=False, view_channel=False,
+                                                                      view_guild_insights=False)
+
+
 
 def setup(bot):
     bot.add_cog(WebhookDelete(bot))
