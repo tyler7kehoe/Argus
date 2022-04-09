1ad69e87104304e8243cb6d6999faf78b241f6e2
tyler7kehoe <kehoe.tyler@att.net>
2022-04-09 12:41:10 -0500

    Update setup.py

--- cogs/setup.py
+++ cogs/setup.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands.context import Context
+from asyncio import sleep
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,6 +11,7 @@ class Setup(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
 
+
+
 def setup(bot):
     bot.add_cog(Setup(bot))
-    
