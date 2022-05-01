b10f99987aa60282ceae50cbc67fa29329a65da0
jacksongski <jacksongski@icloud.com>
2022-05-01 14:42:53 -0500

    opensea works nice

--- cogs/opensea_task.py
+++ cogs/opensea_task.py
@@ -15,7 +15,6 @@ import time as t
 
 
 load_dotenv()
-# TODO its posting duplicates - fix id check thing
 
  
 class Opensea_task(commands.Cog):
@@ -41,8 +40,6 @@ class Opensea_task(commands.Cog):
             gu_id = guild['guild_id']
             guild_obj = self.bot.get_guild(gu_id)
             contracts = guild['contracts']
-            print()
-            print("Time 1 then 2:")
             for contract in contracts:
                 chid = contract['channel_id']
                 ch = guild_obj.get_channel(chid)
@@ -57,52 +54,39 @@ class Opensea_task(commands.Cog):
                 "occurred_after": time - datetime.timedelta(seconds=180)
                 } 
                 newtime = str(datetime.datetime.utcnow())
-                print(time)  
-                print(newtime)     
-                print()       
+                  
                 addr = requests.get("https://api.opensea.io/api/v1/events?event_type=successful", params=params, headers=self.headers)
                 while addr.status_code == 503:
                     addr = requests.get("https://api.opensea.io/api/v1/events?event_type=successful", params=params, headers=self.headers)
                     print("error 503")
 
                 contract['time'] = newtime
-
-                # print(addr)
                 addr = addr.json()
-                # print(addr)
-
                 addr = addr['asset_events']
                 
                 index = 1
                 for i in range(len(addr)-1, -1, -1):
                     index += 1
-                    print(addr[i]['id'])
-                    print(contract['lastId'])
                     if addr[i]['id'] == contract['lastId']:
-                        print("yes")
                         break
                     if i == 0:
                         index = 1
-
 
                 for i in range(len(addr)-index, -1, -1):
                     title = addr[i]['asset']['name']
                     description = addr[i]['asset']['asset_contract']["name"]
                     img = addr[i]['asset']['image_url']
-                    from_acc = addr[i]['transaction']['from_account']['address']
-                    to_acc = addr[i]['winner_account']['address']
+                    seller = addr[i]['transaction']['to_account']['address']
+                    buyer = addr[i]['transaction']['from_account']['address']
 
                     embed = discord.Embed(title=f"{title}", color=discord.Color.blue(),
                                     description=f'{description}')
-                    embed.add_field(name='Seller:', value=f'[{from_acc[:-35]}](https://etherscan.io/address/{from_acc})')
-                    embed.add_field(name='Buyer:', value=f'[{to_acc[:-35]}](https://etherscan.io/address/{to_acc})')     # trouble reaching buyer location with indexing.
-                    # embed.timestamp = datetime.datetime.now()
+                    embed.add_field(name='Seller:', value=f'[{seller[:-35]}](https://etherscan.io/address/{seller})')
+                    embed.add_field(name='Buyer:', value=f'[{buyer[:-35]}](https://etherscan.io/address/{buyer})')     # trouble reaching buyer location with indexing.
                     timestamp = parser.parse(addr[i]["event_timestamp"])
-                    # offset = datetime.datetime.fromtimestamp(timestamp) - datetime.datetime.utcfromtimestamp(timestamp)
                     now = t.time()
                     offset = datetime.datetime.fromtimestamp(now) - datetime.datetime.utcfromtimestamp(now)
                     saletime = timestamp + offset
-                    # embed.timestamp = parser.parse(addr[i]["event_timestamp"]).astimezone(tzlocal())
                     embed.timestamp = saletime
                     embed.set_image(url=img)
                     await ch.send(embed=embed)
