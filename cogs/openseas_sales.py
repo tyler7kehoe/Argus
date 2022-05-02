8ea1ba37eafa99e2661145a0166fbc877a2926c8
tyler7kehoe <kehoe.tyler@att.net>
2022-05-02 12:32:58 -0500

    Added /opensea_remove_contract
    Removes a contract from the json data base

--- cogs/openseas_sales.py
+++ cogs/openseas_sales.py
@@ -26,6 +26,27 @@ class Openseas_Sales(commands.Cog):
         else:
             await ctx.respond("This contract is already being tracked in this channel.")
 
+    @commands.has_permissions(manage_webhooks=True)
+    @commands.slash_command(name="opensea_remove_contract",
+                            description="Remove a contract from being tracked")
+    async def opensea_remove(self, ctx: Context, contract_address):
+        guild_id = ctx.guild.id
+        found = False
+        with open("data/opensea.json", "r", encoding="UTF-8") as _:
+            data = json.load(_)
+            for item in data:
+                if item['guild_id'] == guild_id:
+                    for contract in item['contracts']:
+                        if contract['contract_address'] == contract_address:
+                            item['contracts'].remove(contract)
+                            await ctx.respond(f'Contract: {contract_address} removed')
+                            found = True
+        with open("data/opensea.json", "w", encoding="UTF-8") as _:
+            json.dump(obj=data, fp=_, indent=4)
+        if not found:
+            ctx.respond(f'Contract: {contract_address} not found')
+
+
 
     def log_contract_tracker(self, guild_id, channel_id, contract_address):
         with open("data/opensea.json", "r", encoding="UTF-8") as _:
